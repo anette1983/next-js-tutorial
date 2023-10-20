9aa7d4f763d7ad9ce5c633d0deec6de69a4e8d9f
Hanna <anna_kon@bigmir.net>
2023-10-20 20:36:11 +0300

    added pages, navigation, css, in progress

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Layout, { siteTitle, name } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello! I am <span className={utilStyles.headingLg}>{name}</span>. I am
+          a front-end developer. You can contact me on{" "}
+          <a href="https://www.linkedin.com/in/hanna-konchakovska/">Linkedin</a>.{" "}
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
