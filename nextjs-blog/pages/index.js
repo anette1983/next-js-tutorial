68aaa7e22558f7da458cc5f9f8d2678b6973d5df
Hanna <anna_kon@bigmir.net>
2023-10-20 20:49:34 +0300

    corrections

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle, name } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
         <p>
           Hello! I am <span className={utilStyles.headingLg}>{name}</span>. I am
           a front-end developer. You can contact me on{" "}
-          <a href="https://www.linkedin.com/in/hanna-konchakovska/">Linkedin</a>.{" "}
+          <a href="https://www.linkedin.com/in/hanna-konchakovska/">Linkedin</a>
+          . Read <Link href="/posts/first-post">this page!</Link>
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
